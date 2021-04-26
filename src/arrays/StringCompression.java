681e01bbd614782b8f4a098c42f5fc8fdbd5fd96
shravya-byri <shravya.byri@gmail.com>
2021-04-26 11:36:30 +0530

    ctci 1.6 string compression

---
+++ src/arrays/StringCompression.java
@@ -0,0 +1,35 @@
+package arrays;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        String s = "aabbbccc";
+        System.out.println(updateCompressedString(s));
+    }
+    static String updateCompressedString(String s){
+//        String s2 = "";
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while(j < s.length()){
+            if (s.charAt(i) == s.charAt(j)) {
+                count++;
+                j++;
+            }else {
+                sb.append(s.charAt(i) + count);
+//                s2 = s2 + s.charAt(i)+ count;
+                i = j;
+                count = 0;
+            }
+        }
+        if (count > 0) {
+//            s2 = s2 + s.charAt(j-1) + count;
+            sb.append(s.charAt(j-1) + count);
+        }
+
+        if (s.length() > sb.length()){
+            return sb.toString();
+        } else return s;
+
+    }
+}
