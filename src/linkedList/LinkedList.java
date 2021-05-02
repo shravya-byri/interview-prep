fa3fa9750c7b8bdbf561f0616c6db7fb71cae022
shravya-byri <shravya.byri@gmail.com>
2021-05-02 21:02:26 +0530

    linked list ctci

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -10,18 +10,17 @@ public class LinkedList {
         }
     }
 
-    public static Node newLL(int d) {
-        Node r = new Node(d, null);
-        return r;
-    }
 
-    public static void insert(int n, Node head) {
+    public static Node insert(int n, Node head) {
+        if ( head == null) {
+            return new Node(n, null);
+        }
         Node current = head;
         while (current.next != null) {
             current = current.next;
         }
         current.next = new Node (n, null);
-
+        return head;
     }
     public static void printLL(Node head) {
         Node curr = head;
@@ -33,6 +32,7 @@ public class LinkedList {
         System.out.println();
 
     }
+
     public static Node delete(Node head, int n) {
         if ( n == 0){
             head = head.next;
@@ -47,17 +47,33 @@ public class LinkedList {
         return head;
     }
 
+    public static Node getMiddleNode(Node head){
+        int count = 0;
+        Node current1 = head;
+        Node current2 = head;
+        Node result;
+        while (current1 != null){
+            count = count + 1;
+            current1 = current1.next;
+        }
+        for (int i = 0; i < (count)/2; i++){
+            current2 = current2.next;
+        }
+        result = current2;
+        return result;
+    }
 
     public static void main(String[] args) {
-       Node head = newLL(5);
+       Node head = insert(5,null);
         insert(6, head);
         insert(7,head);
         insert (8, head);
-        insert(9, head);
+//        insert(9, head);
         insert(10, head);
         printLL(head);
-        Node p = delete(head,2);
-        printLL(p);
+//        Node p = delete(head,2);
+//        printLL(p);
+        System.out.print(getMiddleNode(head).data);
 
     }
 }
