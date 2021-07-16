7132aeac5396f73655b21c5a01b75fc3802b39b4
shravya-byri <shravya.byri@gmail.com>
2021-07-16 11:09:23 +0530

    two sum

--- src/arrays/TargetSumPairExists.java
+++ src/arrays/TargetSumPairExists.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.HashMap;
+
 public class TargetSumPairExists {
     public static boolean doesTargetSumExistBruteForce(int[] a, int targetSum) {
         for(int i = 0; i < a.length; i++){
@@ -11,9 +13,21 @@ public class TargetSumPairExists {
         }
         return false;
     }
+    public static boolean targetSumExist(int[] a, int targetSum) {
+        HashMap<Integer,Integer> h = new HashMap<>();
+        for(int i = 0; i < a.length; i++) {
+            if (h.get(a[i]) == null) {
+                if (h.get(targetSum-a[i]) != null){
+                    return true;
+                }
+                h.put(a[i], a[i]);
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int[] a = {1,2,3,4,-2,5,0};
-        int t = 7;
+        int t = 11;
         System.out.println(doesTargetSumExistBruteForce(a,t));
 
     }
