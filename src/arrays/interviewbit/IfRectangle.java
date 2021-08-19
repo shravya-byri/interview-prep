f06c3f80d4bdf4d11f093aaed1ac5b5c709f66f0
shravya-byri <shravya.byri@gmail.com>
2021-08-19 23:18:58 +0530

    rectangle

---
+++ src/arrays/interviewbit/IfRectangle.java
@@ -0,0 +1,19 @@
+package arrays.interviewbit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class IfRectangle {
+    public static void main(String[] args) {
+        int A = 1, B = 2, C = 2, D = 2;
+        System.out.println(Solve(A, B, C, D));
+    }
+
+    public static int Solve(int A, int B, int C, int D) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+
+        return 1;
+    }
+}
+
