8fab2534c31371498aee4c06814716adffeb7316
shravya-byri <shravya.byri@gmail.com>
2021-08-23 21:36:50 +0530

    pascal triangle

--- src/arrays/interviewbit/PascalsTriangle.java
+++ src/arrays/interviewbit/PascalsTriangle.java
@@ -1,2 +1,40 @@
-package arrays.interviewbit;public class PascalsTriangle {
+package arrays.interviewbit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        int n = 11;
+       // System.out.println(Solve(n));
+        Solve(n);
+    }
+    public static ArrayList<ArrayList<Integer>> Solve(int n) {
+        ArrayList<Integer> one = new ArrayList<>();
+        ArrayList<Integer> two = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        one.add(1);
+        two.add(1);
+        two.add(1);
+        ArrayList<Integer> prev = new ArrayList<>(two);
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                res.add(one);
+            } else if (i == 2) {
+                res.add(two);
+            } else {
+                ArrayList<Integer> temp = new ArrayList<>();
+                temp.add(1);
+                for (int j = 0; j < prev.size() - 1; j++) {
+                    temp.add( prev.get(j) + prev.get(j+1) );
+                }
+                temp.add(1);
+                prev = new ArrayList<>(temp);
+                res.add(temp);
+            }
+        }
+        return res;
+    }
 }
