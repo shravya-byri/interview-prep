39887aaf917f74c30ad76b45d4f7907fc11c36ad
shravya-byri <shravya.byri@gmail.com>
2021-08-29 18:04:52 +0530

    palindrome number

--- src/maths/PalindromeNumber.java
+++ src/maths/PalindromeNumber.java
@@ -1,2 +1,20 @@
-package maths;public class PalindromeNumber {
+package maths;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int A = -111;
+        System.out.println(solution(A));
+    }
+    public static int solution(int A) {
+        int num = A;
+        int rev = 0;
+        if(A < 10) return 0;
+        while(A>0) {
+            rev = rev*10 + (A % 10);
+            A = A/10;
+            //System.out.println(rev);
+        }
+        if ( num == rev) return 1;
+        else return 0;
+    }
 }
