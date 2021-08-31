cfe5e28fc2161da964d4b999e4ef188516e10692
shravya-byri <shravya.byri@gmail.com>
2021-08-31 18:52:00 +0530

    firt missing integr

--- src/arrays/interviewbit/FirstMissingInteger.java
+++ src/arrays/interviewbit/FirstMissingInteger.java
@@ -6,13 +6,33 @@ import java.util.List;
 
 public class FirstMissingInteger {
     public static void main(String[] args) {
-        ArrayList<Integer> A = new ArrayList(
-                List.of(1, 4 , 3 , 5)
-        );
+        ArrayList<Integer> A = new ArrayList<Integer>(List.of(2,6,7,9));
         System.out.println(solution(A));
     }
 
     public static int solution(ArrayList<Integer> A) {
-          return 0;
+        int n = A.size();
+        for (int i = 0; i < n; i++) {
+            int currPos = A.get(i)-1;
+            while( 1 <= A.get(i) && A.get(i) <= n && A.get(i) != A.get(currPos)){
+                swap(A,i,currPos);
+                currPos = A.get(i)-1;
+            }
+
+        }
+        for ( int i = 0; i < n; i++) {
+            if(A.get(i) != i+1) {
+                return i+1;
+            }
+        }
+
+        return n+1;
+    }
+
+    public static void swap(ArrayList<Integer> A, int l, int r) {
+        int temp;
+        temp = A.get(l);
+        A.set(l, A.get(r));
+        A.set(r, temp);
     }
 }
