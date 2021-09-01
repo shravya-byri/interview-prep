cc938e494a067da6cdca8b0ec6ab06f9c4d8a4b4
shravya-byri <shravya.byri@gmail.com>
2021-09-01 18:47:42 +0530

    easy method

---
+++ src/arrays/HotelBookingsPossible.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HotelBookingsPossible {
+    public static void main(String[] args) {
+        ArrayList<Integer> A = new ArrayList<>(List.of(1,3,5));
+        ArrayList<Integer> D = new ArrayList<>(List.of(2,5,8));
+        int C = 1;
+        System.out.println(isPossible(A,D,C));
+    }
+    public static boolean isPossible(ArrayList<Integer> A,ArrayList<Integer> D, int C) {
+
+        Collections.sort(A);
+        Collections.sort(D);
+         for(int i = C; i < A.size(); i++) {
+             if(A.get(i) <= D.get(i-C)) {
+                 return false;
+             }
+         }
+        return true;
+    }
+}
