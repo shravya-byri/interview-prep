49dc4532911c50afca5b4672deff7a8943ab6666
shravya-byri <shravya.byri@gmail.com>
2021-10-04 14:46:48 +0530

    revisit

---
+++ src/strings/MinimumParenthesis.java
@@ -0,0 +1,31 @@
+package strings;
+
+public class MinimumParenthesis {
+    public static void main(String[] args) {
+        String S = "())))))))))";
+        System.out.println(solve(S));
+    }
+    public static int solve(String A) {
+        int k = 0;
+        int count = 0;
+        int count1 = 0;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) == '(') {
+                count++;
+            } else {
+                if (A.charAt(i) == ')') {
+                    if (count == k) {
+                        k++;
+                        count = k;
+                    } else{
+                        count--;
+                        count1++;
+                    }
+                }
+
+            }
+
+        }
+        return count1;
+    }
+}
