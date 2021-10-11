3850918615a4642c584e5016c3642691d644c6d6
shravya-byri <shravya.byri@gmail.com>
2021-10-11 22:00:56 +0530

    amazing substring

--- src/strings/AmazingSubString.java
+++ src/strings/AmazingSubString.java
@@ -1,2 +1,35 @@
-package strings;public class AmazingSubString {
+package strings;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class AmazingSubString {
+    public static void main(String[] args) {
+        String A = "AzZGBauYuTknYjjWEEbLvqMQwnoSgXKBdHKEVpeoxYNNtBfrxQrPauttMzjKFayKwMeuChjzCocgAHfAmPCatOqarzLuabyTnxoheeocVshfERNssVPfRyPmwvOVGbzNAuvryYNWwIeyZLMlLbkcFFJRHjEIgIwOThRJJFpLbunVrbhAYsMtdsKslLAGElwrZjvZweIytMpPEYVmktQeNleNROcTjrNxXeHvOMMTMfqZHTUjetojHFzZwOekfAILYISANxeJFRNgeZDKoOTddXqxduPIjGXsRSSkgIqKMeSqlQwAKtdrkvHLgmKleNAPEztGMPmZzUuCImLAhzUnXmsVzFvJUTeIKleuRnMPNAPWJYAZLXgzTBPmkXVShbBSlIAJSeFgvRFvcoqfVFgHUefxUxuYFCfUxbxlOslUhYRFXKmMmqDIQhhfXyGqUwGMSYeLLpEsKAhvFUzavDCOUgtmmNMnsvfmCdPWuWIjuUfZCBTrWnaDopbqXcjzSqRMpQWIBNnMcOQZjDkjPkxsuwENYQyjgSHFJrgSLnwbEInBfdeIfBbVuZZbBrblJgKHOmLZACLQkSRxxQJeUMPIQutraxFtrRcSeqAejOTSqaFGglQOoWPkcNOnLIgfclWNtjGQRVMlqCPUnUlOLbHfkzUyNAmTsswWtZjIGUBrLmRmstHgVcRUgWdQTCEPRzjPVTMJRJocYHftwoRzOSyQexjmceRHdqFdgNuGmGTUdXQaNwKmvOUzZPzGCBVcbVLgMoQrESbpVGteVVntOwEWxXsZnSAoIfBSsWVhDFBuDTkcrnsPdmmSHymouxHlcgtjgKUAPznxsIRUjDFsrjadJjEtPaWTVBHpatqYeSgrpWJDOGfgIGQPcTIXVsCVyCfKMpcXWGkvwuRuTmvCbNMLeUkZrEpYZdlKAgFELfwCCbZCYBcXhfUrsIHfdwhYyxHKAMYMERwlyRtxObDoxBhjXmynYkmkYZrkzlCuvrhW";
+        System.out.println(solve(A));
+    }
+    public static int solve(String A) {
+        int count = 0;
+        char z;
+        int res = 0;
+        String m = A.toLowerCase(Locale.ROOT);
+        ArrayList<Integer> values = new ArrayList<>();
+        for ( int i = 0; i < A.length();i++) {
+            z = m.charAt(i);
+            if (z == 'a' || z == 'e' || z == 'i' || z == 'o' || z == 'u') {
+                count++;
+                values.add(i);
+            }
+        }
+        res = count;
+        int num = 0;
+        int num1 = 0;
+        for ( int i = 0; i < values.size(); i++) {
+            num = A.length()-1- values.get(i);
+            num1 = num1 + num;
+        }
+        res = count + num1;
+        return res% 10003;
+    }
 }
+
