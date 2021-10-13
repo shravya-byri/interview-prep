0dac5e123538ec67cbf9846a6f05f375e9aef96e
shravya-byri <shravya.byri@gmail.com>
2021-10-13 18:48:52 +0530

    atoi revisit

---
+++ src/strings/Atoi.java
@@ -0,0 +1,40 @@
+package strings;
+
+public class Atoi {
+    public static void main(String[] args) {
+        String A = "12aa 1";
+        System.out.println(solve(A));
+    }
+
+    public static int solve(String A) {
+        int i = 0, length = A.length();
+        long num = 0;
+        boolean sign = true;
+
+        while (i < length && A.charAt(i) == ' ')        // get rid of white space
+            ++i;
+
+        if (i == A.length())
+            return 0;    // just have white space
+
+        if (A.charAt(i) == '-') {        // negative
+            sign = false;
+            ++i;
+        } else if (A.charAt(i) == '+')    // positive
+            ++i;
+
+        while (i < A.length() && A.charAt(i) >= '0' && A.charAt(i) <= '9') {
+            num = Math.abs(num);
+            num = num * 10 + (A.charAt(i) - '0');
+
+            if (!sign)
+                num = -num;
+            if (num > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            else if (num < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+            ++i;
+        }
+        return (int) num;
+    }
+}
