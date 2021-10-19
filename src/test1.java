14e3fcd679a55de311b4259d7a7280817f901a3a
shravya-byri <shravya.byri@gmail.com>
2021-10-19 06:48:21 +0530

    scaler

--- src/test1.java
+++ src/test1.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class test1 {
+public class test1 {
+    public static void main(String[] args) {
+        String A = "aabababa";
+        String B = "ba";
+        System.out.println(solve(A,B)) ;
+    }
+    public static int solve(String A, String B) {
+        for ( int i = 0; i < A.length(); i = i + B.length()) {
+            if (isPresent(A,B,i,i+B.length()-1)){
+                return i+1;
+            }
+
+        }
+        return -1;
+
+
+    }
+    public static boolean isPresent(String A, String B,int start, int end) {
+        int j = 0;
+        int i = 0;
+        for(i = start; i < end; i++) {
+            if (A.charAt(i) == B.charAt(j)) {
+                j++;
+            } else return false;
+        }
+        return true;
+
+
+    }
+
 }
