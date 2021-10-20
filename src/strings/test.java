94620c2e1dcf6cea98e86c1fcb914836d5ad8bb8
shravya-byri <shravya.byri@gmail.com>
2021-10-20 20:12:40 +0530

    scaler

--- src/strings/test.java
+++ src/strings/test.java
@@ -1,2 +1,26 @@
-package strings;public class test {
+package strings;
+
+public class test {
+    public static void main(String[] args) {
+        String A = "abbca";
+        System.out.println(solve(A));
+    }
+    public static int solve(String A) {
+
+        int l = A.length(), prev=0;
+        long res=0;
+        boolean flag=true;
+        for(int i = 0; i < l; i++) {
+            char c = A.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                res+=(i-prev++);
+            }else {
+                res+=prev;
+            }
+            res%=1000000007;
+            // System.out.println(i+":"+res);
+        }
+        return (int)res;
+
+    }
 }
